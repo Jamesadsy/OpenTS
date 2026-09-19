15f097cfa9300933fd27b8de1fa9d904d5917216
Jamesadsy <jamesadsy@hotmail.co.uk>
2026-09-19 09:49:14 +0100

    Fix Visual C header guard after reconciliation

--- code/visualc.h
+++ code/visualc.h
@@ -148,8 +148,6 @@
 #define M_FPI 3.141592654f
 #endif
 
-#endif
-
 // Spellings no C library supplies, so they are needed whatever the compiler.
 #ifndef M_SQRT_2
 #define M_SQRT_2    0.707106781186547524401
